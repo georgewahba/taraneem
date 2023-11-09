c9aba0141d9ca81b9a66ea2186970fa593913ffe
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-11-09 04:00:30 +0100

    mobile

--- resources/views/showtaraneem.blade.php
+++ resources/views/showtaraneem.blade.php
@@ -88,7 +88,7 @@
         document.getElementById("visibletext").innerHTML = textdisplay.join("<br>"); // Join with <br> to display sentences on separate lines
 
         document.addEventListener('keydown', function (event) {
-            if (event.keyCode == 39) {
+            if (event.keyCode == 39 || event.keyCode == 40  ) {
             i++;
             if (i >= textarray.length) {
                 i = textarray.length - 1;
@@ -97,7 +97,7 @@
             document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
         }
 
-            if (event.keyCode == 37) {
+            if (event.keyCode == 37 || event.keyCode == 38) {
             i--;
             if (i < 0) {
                 i = 0;
@@ -106,6 +106,77 @@
             document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
         }
         });
+
+
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        var xDown = null;                                                        
+        var yDown = null;
+
+        function getTouches(evt) {
+        return evt.touches ||             // browser API
+                evt.originalEvent.touches; // jQuery
+        }                                                     
+                                                                                
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];                                      
+            xDown = firstTouch.clientX;                                      
+            yDown = firstTouch.clientY;                                      
+        };                                                
+                                                                                
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+                                                                                
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    i++;
+                    if (i >= textarray.length) {
+                        i = textarray.length - 1;
+                    }
+                    textdisplay = textarray[i].split("@");
+                    document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
+                } else {
+                    i--
+                    if (i < 0) {
+                        i = 0;
+                    }
+                    textdisplay = textarray[i].split("@");
+                    document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
+                }                       
+            } else {
+                if ( yDiff > 0 ) {
+                    i--
+                    if (i < 0) {
+                        i = 0;
+                    }
+                    textdisplay = textarray[i].split("@");
+                    document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
+                } else { 
+                    i++;
+                    if (i >= textarray.length) {
+                        i = textarray.length - 1;
+                    }
+                    textdisplay = textarray[i].split("@");
+                    document.getElementById("visibletext").innerHTML = textdisplay.join("<br>");
+                }                                                                 
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
+        };
+
+
+
+
     </script>
 </body>
 </html>
